3e7b2517466ffe65f8a86fca499ad5aff0bf9eba
Eugene Morozov <eugene.morozov@gmail.com>
2015-02-21 13:35:04 +0530

    Fixed usage of the deprecated simplejson module.

--- oauth_access/access.py
+++ oauth_access/access.py
@@ -1,4 +1,5 @@
 import cgi
+import json
 import datetime
 import httplib2
 import logging
@@ -12,7 +13,6 @@ except ImportError:
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
-from django.utils import simplejson as json
 
 from django.contrib.sites.models import Site
 
@@ -28,39 +28,39 @@ logger = logging.getLogger("oauth_access.access")
 
 
 class OAuthAccess(object):
-    
+
     def __init__(self, service):
         self.service = service
         self.signature_method = oauth.SignatureMethod_HMAC_SHA1()
         self.consumer = oauth.Consumer(self.key, self.secret)
-    
+
     @property
     def key(self):
         return self._obtain_setting("keys", "KEY")
-    
+
     @property
     def secret(self):
         return self._obtain_setting("keys", "SECRET")
-    
+
     @property
     def request_token_url(self):
         return self._obtain_setting("endpoints", "request_token")
-    
+
     @property
     def access_token_url(self):
         return self._obtain_setting("endpoints", "access_token")
-    
+
     @property
     def authorize_url(self):
         return self._obtain_setting("endpoints", "authorize")
-    
+
     @property
     def provider_scope(self):
         try:
             return self._obtain_setting("endpoints", "provider_scope")
         except KeyError:
             return None
-    
+
     def _obtain_setting(self, k1, k2):
         name = "OAUTH_ACCESS_SETTINGS"
         service = self.service
@@ -78,12 +78,12 @@ class OAuthAccess(object):
                 raise ImproperlyConfigured("%s must contain '%s' for '%s' in '%s'" % (name, k2, k1, service))
             else:
                 raise
-    
+
     def unauthorized_token(self):
         if not hasattr(self, "_unauthorized_token"):
             self._unauthorized_token = self.fetch_unauthorized_token()
         return self._unauthorized_token
-    
+
     def fetch_unauthorized_token(self):
         parameters = {
             "oauth_callback": self.callback_url,
@@ -104,7 +104,7 @@ class OAuthAccess(object):
                     response["status"], self.request_token_url, content
                 ))
         return oauth.Token.from_string(content)
-    
+
     @property
     def callback_url(self):
         current_site = Site.objects.get(pk=settings.SITE_ID)
@@ -114,7 +114,7 @@ class OAuthAccess(object):
             "service": self.service,
         })
         return "%s%s" % (base_url, callback_url)
-    
+
     def authorized_token(self, token, verifier=None):
         parameters = {}
         if verifier:
@@ -134,7 +134,7 @@ class OAuthAccess(object):
                     response["status"], self.access_token_url, content
                 ))
         return oauth.Token.from_string(content)
-    
+
     def check_token(self, unauth_token, parameters):
         if self.service != "facebook" and unauth_token is None:
             raise MissingToken
@@ -174,11 +174,11 @@ class OAuthAccess(object):
             else:
                 # @@@ this error case is not nice
                 return None
-    
+
     @property
     def callback(self):
         return load_path_attr(self._obtain_setting("endpoints", "callback"))
-    
+
     def authorization_url(self, token=None):
         if token is None:
             # OAuth 2.0
@@ -198,7 +198,7 @@ class OAuthAccess(object):
             )
             request.sign_request(self.signature_method, self.consumer, token)
             return request.to_url()
-    
+
     def persist(self, user, token, identifier=None):
         expires = hasattr(token, "expires") and token.expires or None
         defaults = {
@@ -216,7 +216,7 @@ class OAuthAccess(object):
             assoc.token = str(token)
             assoc.expires = expires
             assoc.save()
-    
+
     def lookup_user(self, identifier):
         queryset = UserAssociation.objects.all()
         queryset = queryset.select_related("user")
@@ -227,7 +227,7 @@ class OAuthAccess(object):
             return None
         else:
             return assoc.user
-    
+
     def make_api_call(self, kind, url, token, method="GET", headers=None, **kwargs):
         if isinstance(token, OAuth20Token):
             request_kwargs = dict(method=method)
@@ -281,32 +281,32 @@ class Client(oauth.Client):
     Custom client to support forcing Authorization header (which is required
     by LinkedIn). See http://github.com/brosner/python-oauth2/commit/82a05f96878f187f67c1af44befc1bec562e5c1f
     """
-    
+
     def request(self, uri, method="GET", body=None, headers=None,
       redirections=httplib2.DEFAULT_MAX_REDIRECTS, connection_type=None,
       force_auth_header=False):
-        
+
         DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded"
-        
+
         if not isinstance(headers, dict):
             headers = {}
-        
+
         is_multipart = method == "POST" and headers.get("Content-Type", DEFAULT_CONTENT_TYPE) != DEFAULT_CONTENT_TYPE
-        
+
         if body and method == "POST" and not is_multipart:
             parameters = dict(parse_qsl(body))
         else:
             parameters = None
-        
+
         req = oauth.Request.from_consumer_and_token(self.consumer,
             token=self.token, http_method=method, http_url=uri,
             parameters=parameters)
-        
+
         req.sign_request(self.method, self.consumer, self.token)
-        
+
         if force_auth_header:
             headers.update(req.to_header())
-        
+
         if method == "POST":
             headers["Content-Type"] = headers.get("Content-Type", DEFAULT_CONTENT_TYPE)
             if is_multipart:
@@ -323,20 +323,20 @@ class Client(oauth.Client):
             if not force_auth_header:
                 # don't call update twice.
                 headers.update(req.to_header())
-        
+
         return httplib2.Http.request(self, uri, method=method, body=body,
             headers=headers, redirections=redirections,
             connection_type=connection_type)
 
 
 class OAuth20Token(object):
-    
+
     def __init__(self, token, expires=None):
         self.token = token
         if expires is not None:
             self.expires = datetime.datetime.now() + datetime.timedelta(seconds=expires)
         else:
             self.expires = None
-    
+
     def __str__(self):
         return str(self.token)
