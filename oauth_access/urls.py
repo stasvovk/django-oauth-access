785ef5d1a2c751ceb3be3c8db334f8d8212ed610
Eugene Morozov <eugene.morozov@gmail.com>
2016-08-30 19:55:57 +0300

    Updated urls for Django 1.8+.

--- oauth_access/urls.py
+++ oauth_access/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns("",
+urlpatterns = [
     url(r"^login/(?P<service>\w+)/$", views.oauth_login, name="oauth_access_login"),
     url(r"^callback/(?P<service>\w+)/$", views.oauth_callback, name="oauth_access_callback"),
     url(r"^finish_signup/(?P<service>\w+)/$", views.finish_signup, name="oauth_access_finish_signup")
-)
+]
 
